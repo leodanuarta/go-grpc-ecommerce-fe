8c385151ff9619d7158b2ed2b4509b3aa5ef2f2a
leodanuarta <leo.535210024@stu.untar.ac.id>
2025-06-29 23:40:25 +0700

    Leo 29/06/25 control form

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,6 +1,31 @@
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import * as yup from 'yup';
 
+const loginSchema = yup.object().shape({
+    email: yup.string().email('Email tidak valid').required('Email wajid diisi'),
+    password: yup.string().required('Password wajid diisi').min(6, 'Password minimal 6 karakter'),
+})
+
+interface LoginFormValues{
+    email: string;
+    password: string;
+}
 const Login = () => {
+    const form = useForm<LoginFormValues>({
+        resolver: yupResolver(loginSchema),
+    });
+
+    const submitHandler = (values: LoginFormValues) => {
+        console.log(values)
+        Swal.fire({
+            icon: 'success',
+            title: 'Login Success',
+            confirmButtonText: 'OK',
+        })
+    }
     return (
         <div className="login-section">
             <div className="container">
@@ -8,12 +33,14 @@ const Login = () => {
                     <div className="col-md-6 col-lg-5">
                         <div className="login-wrap p-4">
                             <h2 className="section-title text-center mb-5">Masuk</h2>
-                            <form action="#" className="login-form">
+                            <form onSubmit={form.handleSubmit(submitHandler)} action="#" className="login-form">
                                 <div className="form-group mb-4">
-                                    <input type="email" className="form-control" placeholder="Alamat Email" required />
+                                    <input type="text" className={`form-control ${form.formState.errors.email ? 'is-invalid' : ''}`} placeholder="Alamat Email" {...form.register('email')} />
+                                    <div className={`text-danger ${form.formState.errors.email ? '' : 'hidden'}`} style={{height: 8}}><small>{form.formState.errors.email?.message ?? ''}</small></div>
                                 </div>
                                 <div className="form-group mb-4">
-                                    <input type="password" className="form-control" placeholder="Kata Sandi" required />
+                                    <input type="password" className={`form-control ${form.formState.errors.password ? 'is-invalid' : ''}`} placeholder="Kata Sandi"{...form.register('password')} />
+                                    <div className={`text-danger ${form.formState.errors.password ? '' : 'hidden'}`} style={{height: 8}}><small>{form.formState.errors.password?.message ?? ''}</small></div>
                                 </div>
                                 <div className="form-group">
                                     <button type="submit" className="btn btn-primary btn-block">Masuk</button>
