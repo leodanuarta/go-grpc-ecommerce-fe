7a2d5c0d019aa2f48269bb85d5ba9d9fdfb04bfe
leodanuarta <leo.535210024@stu.untar.ac.id>
2025-07-04 00:04:23 +0700

    Leo 04/07/25 integrasi registrasi user

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,6 +1,77 @@
-import { Link } from 'react-router-dom';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link, useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import * as yup from 'yup';
+import { getAuthClient } from '../../api/grpc/client';
+import FormInput from '../../components/FormInput/FormInput';
 
-const Register = () => {
+const registerSchema = yup.object().shape({
+    full_name: yup.string().required('Nama lengkap wajib diisi'),
+    email: yup.string().email('Alamat email tidak valid').required('Alamat email wajib diisi'),
+    password: yup.string().required('Kata sandi wajib diisi').min(6, 'Kata sandi minimal 6 karakter'),
+    password_confirmation: yup.string().required('Kata sandi wajib diisi').oneOf([yup.ref('password')], 'Konfirmasi kata sandi tidak sesuai'),
+})
+
+interface RegisterFormValues {
+    full_name: string;
+    email: string;
+    password: string;
+    password_confirmation: string;
+
+}
+
+const Register =  () => {
+    const navigate = useNavigate()
+    const [submitLoading, setSubmitLoading] = useState<boolean>(false);
+    const form = useForm<RegisterFormValues>({
+        resolver: yupResolver(registerSchema),
+    });
+
+    const submitHandler = async (values: RegisterFormValues) => {
+        try{
+            setSubmitLoading(true)
+            const res = await getAuthClient().register({
+                email: values.email,
+                fullName: values.full_name,
+                password: values.password,
+                passwordConfirmation: values.password_confirmation,
+            });
+
+            if (res.response.base?.isError ?? true){
+                // case email duplicate
+                if (res.response.base?.message === "User already exists"){
+                    Swal.fire({
+                    title: 'Registrasi Gagal',
+                    text: 'Email sudah terdaftar di dalam sistem',
+                    icon: 'error',
+                    })
+
+                    return
+                }
+
+                Swal.fire({
+                    title: 'Terjadi kesalahan',
+                    text: 'Mohon coba beberapa saat lagi',
+                    icon: 'error',
+                })
+                
+                return
+            }
+
+            Swal.fire({
+                title: 'Registrasi Berhasil',
+                text: 'Silahkan masuk menggunakan akun baru Anda',
+                icon: 'success',
+            })
+
+            navigate('/login')
+        }finally{
+            setSubmitLoading(false)
+        }
+       
+    }
     return (
         <div className="login-section">
             <div className="container">
@@ -8,21 +79,41 @@ const Register = () => {
                     <div className="col-md-6 col-lg-5">
                         <div className="login-wrap p-4">
                             <h2 className="section-title text-center mb-5">Daftar</h2>
-                            <form action="#" className="login-form">
-                                <div className="form-group mb-4">
-                                    <input type="text" className="form-control" placeholder="Nama Lengkap" required />
-                                </div>
-                                <div className="form-group mb-4">
-                                    <input type="email" className="form-control" placeholder="Alamat Email" required />
-                                </div>
-                                <div className="form-group mb-4">
-                                    <input type="password" className="form-control" placeholder="Kata Sandi" required />
-                                </div>
-                                <div className="form-group mb-4">
-                                    <input type="password" className="form-control" placeholder="Konfirmasi Kata Sandi" required />
-                                </div>
+                            <form onSubmit={form.handleSubmit(submitHandler)} className="login-form">
+                                <FormInput<RegisterFormValues>
+                                    errors={form.formState.errors}
+                                    name='full_name'
+                                    register={form.register}
+                                    type='text'
+                                    placeholder='Nama Lengkap'
+                                    disabled={submitLoading}
+                                />
+                                <FormInput<RegisterFormValues>
+                                    errors={form.formState.errors}
+                                    name='email'
+                                    register={form.register}
+                                    type='text'
+                                    placeholder='Alamat Email'
+                                    disabled={submitLoading}
+                                />
+                                <FormInput<RegisterFormValues>
+                                    errors={form.formState.errors}
+                                    name='password'
+                                    register={form.register}
+                                    type='password'
+                                    placeholder='Kata Sandi'
+                                    disabled={submitLoading}
+                                />
+                                <FormInput<RegisterFormValues>
+                                    errors={form.formState.errors}
+                                    name='password_confirmation'
+                                    register={form.register}
+                                    type='password'
+                                    placeholder='Konfirmasi Kata Sandi'
+                                    disabled={submitLoading}
+                                />
                                 <div className="form-group">
-                                    <button type="submit" className="btn btn-primary btn-block">Buat Akun</button>
+                                    <button type="submit" className="btn btn-primary btn-block" disabled={submitLoading}>Buat Akun</button>
                                 </div>
                                 <div className="text-center mt-4">
                                     <p>Sudah punya akun? <Link to="/login" className="text-primary">Masuk di sini</Link></p>
