439de52b0646bf2fe653d26e8785943b2a71b45c
leodanuarta <leo.535210024@stu.untar.ac.id>
2025-07-06 17:59:42 +0700

    Leo 06/07/25 create form tambah produk admin

--- src/components/FormInput/FormInput.tsx
+++ src/components/FormInput/FormInput.tsx
@@ -3,29 +3,63 @@ import { FieldErrors, Path, UseFormRegister } from "react-hook-form";
 
 // eslint-disable-next-line
 interface FormInputProps<T extends Record<string, any>>{
-  type: "text" | "password";
+  type: "text" | "password" | "image" | "textarea";
   placeholder?: string;
   register: UseFormRegister<T>;
   name: Path<T>;
   errors: FieldErrors<T>;
   disabled?: boolean;
   label?: string;
+  lableRequired?: boolean;
 }
 
 
 // eslint-disable-next-line
 function FormInput<T extends Record<string, any>>(props: FormInputProps<T>) {
-  return (
-    <div className="form-group mb-4">
-      {props.label && 
-        <label className="text-black" htmlFor={props.name}>{props.label}</label>
-      }
+
+  const renderInput = () => {
+    if (props.type === "image"){
+      return (
+        <input 
+          type="file"
+          accept="image/*"
+          multiple={false}
+          className={`form-control ${props.errors[props.name] ? 'is-invalid' : ''}`} 
+          id={props.name} placeholder={props.placeholder} 
+          disabled={props.disabled} {...props.register(props.name)}
+        />
+      )
+    }
+
+    if (props.type === "textarea"){
+      return (
+        <textarea
+          rows={4} 
+          className={`form-control ${props.errors[props.name] ? 'is-invalid' : ''}`} 
+          id={props.name} placeholder={props.placeholder} 
+          disabled={props.disabled} {...props.register(props.name)}
+        />
+      )
+    }
+    return (
         <input 
           type={props.type} 
           className={`form-control ${props.errors[props.name] ? 'is-invalid' : ''}`} 
           id={props.name} placeholder={props.placeholder} 
           disabled={props.disabled} {...props.register(props.name)}
-          />
+        />
+    )
+  }
+
+  return (
+    <div className="form-group mb-4">
+      {props.label && 
+        <label className="text-black" htmlFor={props.name}>
+          {props.label} {props.lableRequired && <span className="text-danger">*</span>}
+        </label>
+      }
+      
+        {renderInput()}
         <div className={`text-danger ${props.errors[props.name] ? '' : 'hidden'}`} style={{height: 8}}>
           <small>{(props.errors[props.name]?.message as string | null) ?? ''}</small>
         </div>
