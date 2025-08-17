5a8af2c67fd08d37600243f382bd3fb3b094fd07
leodanuarta <leo.535210024@stu.untar.ac.id>
2025-08-17 16:26:45 +0700

    Leo 17/08/25 integrasi API highlight Product FE

--- src/components/ProductHighlightSection/ProductHighlightSection.tsx
+++ src/components/ProductHighlightSection/ProductHighlightSection.tsx
@@ -1,10 +1,38 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { getProductClient } from '../../api/grpc/client';
+import useGrpcApi from '../../hooks/useGrpcApi';
+import { formatToIdr } from '../../utils/number';
 
 interface ProductHighlightSectionProps {
     beforeFooter?: boolean;
 }
 
+interface Product {
+    id: string;
+    name: string;
+    price: number;
+    imageUrl: string;
+}
+
 function ProductHighlightSection(props: ProductHighlightSectionProps) {
+    const productApi = useGrpcApi();
+    const [items, setItems] = useState<Product[]>([]);
+    useEffect(() => {
+        const fethData = async () => {
+            const res = await productApi.callApi(getProductClient().highlightProducts({}));
+
+            setItems(res.response.data.map(d => ({
+                id: d.id,
+                name: d.name,
+                imageUrl: d.imageUrl,
+                price: d.price,
+            })))
+        }
+
+        fethData();
+    }, [])
+
     return (
         <div className={`product-section ${props.beforeFooter ? 'before-footer-section' : ''}`}>
             <div className="container">
@@ -15,41 +43,18 @@ function ProductHighlightSection(props: ProductHighlightSectionProps) {
                         <p><Link to="/shop" className="btn">Jelajahi</Link></p>
                     </div>
 
-                    {/* Product Items */}
-                    <div className="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
-                        <Link className="product-item" to="/cart">
-                            <img src="/images/product-1.png" className="img-fluid product-thumbnail" alt="Nordic Chair" />
-                            <h3 className="product-title">Nordic Chair</h3>
-                            <strong className="product-price">Rp750.000</strong>
-                            <span className="icon-cross">
-                                <img src="/images/cross.svg" className="img-fluid" alt="Cross" />
-                            </span>
-                        </Link>
-                    </div>
-
-                    <div className="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
-                        <Link className="product-item" to="cart.html">
-                            <img src="images/product-2.png" className="img-fluid product-thumbnail" />
-                            <h3 className="product-title">Kruzo Aero Chair</h3>
-                            <strong className="product-price">Rp1.170.000</strong>
-
-                            <span className="icon-cross">
-                                <img src="images/cross.svg" className="img-fluid" />
-                            </span>
-                        </Link>
-                    </div>
-
-                    <div className="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
-                        <Link className="product-item" to="cart.html">
-                            <img src="images/product-3.png" className="img-fluid product-thumbnail" />
-                            <h3 className="product-title">Ergonomic Chair</h3>
-                            <strong className="product-price">Rp645.000</strong>
-
-                            <span className="icon-cross">
-                                <img src="images/cross.svg" className="img-fluid" />
-                            </span>
-                        </Link>
-                    </div>
+                    {items.map(item => (
+                        <div key={item.id} className="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
+                            <Link className="product-item" to="/cart">
+                                <img src={item.imageUrl} className="img-fluid product-thumbnail" alt="product_image" />
+                                <h3 className="product-title">{item.name}</h3>
+                                <strong className="product-price">{formatToIdr(item.price)}</strong>
+                                <span className="icon-cross">
+                                    <img src="/images/cross.svg" className="img-fluid" alt="Cross" />
+                                </span>
+                            </Link>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
